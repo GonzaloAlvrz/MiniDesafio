7ac02362694095047b09c24d9a45631d37d62796
GonzaloAlvrz <gonzalo.alvarez@arbusta.net>
2023-02-01 11:12:07 -0300

    Código final

--- pagina-dc/tailwind.config.js
+++ pagina-dc/tailwind.config.js
@@ -9,19 +9,12 @@ module.exports = {
         'mobile': '360px',
         'mobileM': '475px',
         'mobileL': '595px',
-        // => @media (min-width: 360px) { ... }
         'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
         'lap_tablet': '800px',
-        // => @media (min-width: 768px) { ... }
         'laptop': '1000px',
-        // => @media (min-width: 768px) { ... }
         'desktop-one': '1110px',
-        // => @media (min-width: 1024px) { ... }
-      /*   'desktop-one': '1230px', */
-        // => @media (min-width: 1280px) { ... }
         'desktop-two': '1500px',
-        // => @media (min-width: 1280px) { ... }
+  
       },
     },
   },
